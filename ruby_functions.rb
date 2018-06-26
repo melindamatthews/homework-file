6f5317002d32ed859e5313cc417911b0a09525a5
melinda <melinda.matthews@codeclan.com>
2018-06-26 16:50:08 +0100

    lab homework

---
+++ ruby_functions.rb
@@ -0,0 +1,31 @@
+def return_10
+  return 20 / 2
+end
+
+def add(x,y)
+  return y + x
+end
+
+def subtract(x,y)
+  return x - y
+end
+
+ def multiply(x,y)
+   return x * y
+ end
+
+ def divide(x,y)
+   return x / y
+ end
+
+ def length_of_string(new_string)
+   return new_string.length
+ end
+
+ def join_string(new_string, old_string)
+   return new_string + old_string
+ end
+
+def add_string_as_number(int_as_string_1, int_as_string_2)
+  return x.to_i + y.to_i 
+end

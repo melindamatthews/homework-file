dbe390695541a0d9aea634406295d6c893081974
melinda <melinda.matthews@codeclan.com>
2018-06-26 18:27:55 +0100

    my homework day 2 with tears

--- specs/ruby-functions_spec.rb
+++ specs/ruby-functions_spec.rb
@@ -52,53 +52,56 @@ def test_add()
     add_result = add_string_as_number( "1", "2" )
     assert_equal( 3, add_result )
   end
+
+  def test_number_to_full_name__month_1()
+    result = number_to_full_month_name( 1 )
+    assert_equal( "January", result )
+  end
+
+  def test_number_to_full_name__month_3()
+    result = number_to_full_month_name( 3 )
+    assert_equal( "March", result )
+  end
+
+  def test_number_to_full_name__month_9()
+    result = number_to_full_month_name( 9 )
+    assert_equal( "September", result )
+  end
+
+  def test_substring__month_1()
+    first_month_string = number_to_short_month_name( 1 )
+    assert_equal( "Jan", first_month_string )
+  end
+
+  def test_substring__month_4()
+    fourth_month_string = number_to_short_month_name( 4 )
+    assert_equal( "Apr", fourth_month_string )
+  end
+
+  def test_substring__month_10()
+    tenth_month_string = number_to_short_month_name( 10 )
+    assert_equal( "Oct", tenth_month_string )
+  end
   #
-  # def test_number_to_full_name__month_1()
-  #   result = number_to_full_month_name( 1 )
-  #   assert_equal( "January", result )
-  # end
+  #Further
+
+  #Given the length of a side of a cube calculate the volume
+  def test_volume_of_cube()
+    length_of_side = volume_of_cube(3)
+    assert_equal(27, length_of_side)
+  end
   #
-  # def test_number_to_full_name__month_3()
-  #   result = number_to_full_month_name( 3 )
-  #   assert_equal( "March", result )
-  # end
+  #Given the radius of a sphere calculate the volume
+  def test_volume_of_sphere()
+      length of radius = volume_of_sphere(2)
+      assert_equal(34, volume_of_sphere)
+  end
   #
-  # def test_number_to_full_name__month_9()
-  #   result = number_to_full_month_name( 9 )
-  #   assert_equal( "September", result )
-  # end
-  #
-  # def test_substring__month_1()
-  #   first_month_string = number_to_short_month_name( 1 )
-  #   assert_equal( "Jan", first_month_string )
-  # end
-  #
-  # def test_substring__month_4()
-  #   fourth_month_string = number_to_short_month_name( 4 )
-  #   assert_equal( "Apr", fourth_month_string )
-  # end
-  #
-  # def test_substring__month_10()
-  #   tenth_month_string = number_to_short_month_name( 10 )
-  #   assert_equal( "Oct", tenth_month_string )
-  # end
-  #
-  # #Further
-  #
-  # #Given the length of a side of a cube calculate the volume
-  # def test_volume_of_cube()
-  #   #add test code here
-  # end
-  #
-  # #Given the radius of a sphere calculate the volume
-  # def test_volume_of_sphere()
-  #   #add test code here
-  # end
-  #
-  # #Given a value in farenheit, convert this into celsius.
-  # def test_fahrenheit_to_celsius()
-  #   #add test code here
-  # end
+  #Given a value in farenheit, convert this into celsius.
+  def test_fahrenheit_to_celsius()
+    celsius = temp_of_air(50)
+    assert_equal(10,fahrenheit_to_celsius)
+  end
 
 
 end
